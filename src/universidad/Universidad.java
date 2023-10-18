e79fdb5fa38e31b7e3fd1d7aed508b2afff046fb
LuciaYuhasz <luciayuhasz@gmail.com>
2023-10-18 20:55:23 -0300

    c

--- src/universidad/Universidad.java
+++ src/universidad/Universidad.java
@@ -15,15 +15,21 @@ public class Universidad {
     public static void main(String[] args) {
         
       // Alumno Juan=new Alumno(1,12323,"Luna"," Juan Pedro",LocalDate.of(1980,4,25),true);
-       AlumnoData alu=new AlumnoData();
+       //AlumnoData alu=new AlumnoData();
      //  alu.guardarAlumno(Juan);
      //alu.modificarAlumno(Juan);
      
      //alu.eliminarAlumno(1);
-     Alumno alumnoEncontrado=alu.buscarAlumno(2);
-        System.out.println("dni"+alumnoEncontrado.getDni());
-        System.out.println("apellido"+ alumnoEncontrado.getApellido());
+     //Alumno alumnoEncontrado=alu.buscarAlumnoPorDni(55846254);
+     //if(alumnoEncontrado!=null){
+       // System.out.println("dni"+alumnoEncontrado.getDni());
+        //System.out.println("apellido"+ alumnoEncontrado.getApellido());
+        //}
+        AlumnoData alu=new AlumnoData();
+        for(Alumno alumno:alu.listarAlumnos()){
+            System.out.println(alumno.getDni());
+            System.out.println(alumno.getApellido());
+            System.out.println(alumno.getFechaNac());
         }
-        
-    }
+    }}
 
--- src/universidad/accesoADatos/AlumnoData.java
+++ src/universidad/accesoADatos/AlumnoData.java
@@ -7,6 +7,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -134,5 +136,78 @@ public class AlumnoData {
       return alumno;
   }
   
+  
+  
+  
+  //*************************************************************
+  public Alumno buscarAlumnoPorDni(int dni){
+      String sql="SELECT idAlumno,dni,apellido, nombre,fechaNacimiento FROM alumno WHERE dni =? AND estado=1";
+        Alumno alumno=null;
+      
+      try {
+            PreparedStatement ps= con.prepareStatement(sql);
+            
+            
+            ps.setInt(1,dni);
+            
+            ResultSet rs=ps.executeQuery();
+            
+            if(rs.next()){
+                alumno= new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                alumno.setDni(rs.getInt("dni"));
+                alumno.setApellido(rs.getString("apellido"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setFechaNac(rs.getDate("fechaNacimiento").toLocalDate());
+                alumno.setEstado(true);
+                
+                
+            }else{
+                JOptionPane.showMessageDialog(null,"NO exixte un alumno con ese ID");
+        } 
+      ps.close();
+      
+      
+      }
+      catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error al aceder a la tabla alumno");
+        }
+      
+      return alumno;
+  }
+  
+  //******************************************************
+  public List<Alumno>listarAlumnos(){
+      String sql="SELECT idAlumno,dni,apellido, nombre,fechaNacimiento FROM alumno estado=1";
+       ArrayList<Alumno> alumnos=new ArrayList<>();
+      
+      try {
+            PreparedStatement ps= con.prepareStatement(sql);
+            
+            ResultSet rs=ps.executeQuery();
+            
+            while(rs.next()){
+              
+                Alumno alumno= new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                alumno.setDni(rs.getInt("dni"));
+                alumno.setApellido(rs.getString("apellido"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setFechaNac(rs.getDate("fechaNacimiento").toLocalDate());
+                alumno.setEstado(true);
+                
+                alumnos.add(alumno);
+                
+            } 
+      ps.close();
+      
+      
+      }
+      catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error al aceder a la tabla alumno");
+        }
+      
+      return alumnos;
+  }
   
 }
